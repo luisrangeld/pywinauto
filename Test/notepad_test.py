156863ef75dbc1a2a8b608eebe00e138930f1d7c
luisrangeld <97576051+luisrangeld@users.noreply.github.com>
2025-08-12 09:52:52 -0500

    Update notepad_test.py

--- Test/notepad_test.py
+++ Test/notepad_test.py
@@ -1,26 +1,28 @@
+#for Notepad I do not need to add the path because Calculator is already in Pywinauto for testing purposes
+
 from pywinauto.application import Application
 from pywinauto import Desktop
 from time import sleep
 
-# Paso 1: Inicia el Bloc de notas
+# Start notepad
 app = Application(backend="uia").start("notepad.exe")
 sleep(2)
 
-# Paso 2: Buscar la ventana del Bloc de notas
+# Search the notepad window
 ventana = Desktop(backend='uia').window(title_re= ".*(Bloc de notas|Notepad).*")
 ventana.wait("visible", timeout=10)
 
-# Escribir texto
+# Write text
 ventana.type_keys("¡Hola! Soy Luis ", with_spaces=True)
 sleep(2)
 
-# Hacer click en Editar > Fecha y Hora
+# Click: Editar > Fecha y Hora
 ventana.Editar.click_input()
 sleep(1)
 ventana.child_window(title="Fecha y hora", control_type="MenuItem").click_input()
 sleep(2)
 
-# Hacer Zoom
+# Zoom In
 for i in range (5):
     ventana.Ver.click_input()
     sleep(2)
@@ -28,3 +30,4 @@ for i in range (5):
     ventana.child_window(title="Acercar", control_type="MenuItem").click_input()
     sleep(2)
     
+
